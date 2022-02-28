6620682bff8218d9b542910ffbcaa1836318f2db
epifanio <epiesasha@me.com>
2022-02-28 15:22:21 +0000

    add more apps to entrypoint

--- apps/pybasket_ui/entrypoint.sh
+++ apps/pybasket_ui/entrypoint.sh
@@ -5,4 +5,8 @@ else
     PREFIX_PARAM="--prefix ${PREFIX}";
 fi
 # ${LOG_LEVEL}
-bokeh serve --port ${PORT} --address 0.0.0.0 --allow-websocket-origin ${ORIGIN} ${PREFIX_PARAM} --log-level debug /pybasket_ui
+#bokeh serve --port ${PORT} --address 0.0.0.0 --allow-websocket-origin ${ORIGIN} ${PREFIX_PARAM} --log-level debug /pybasket_ui
+
+# bokeh serve --port ${PORT} --address 0.0.0.0 --allow-websocket-origin '*' ${PREFIX_PARAM} --log-level debug /pybasket_ui /SIE /GISPY /TS-Plot
+
+bokeh serve --port ${PORT} --address 0.0.0.0 --allow-websocket-origin ${ORIGIN} ${PREFIX_PARAM} --log-level debug /pybasket_ui /SIE /GISPY /TS-Plot
