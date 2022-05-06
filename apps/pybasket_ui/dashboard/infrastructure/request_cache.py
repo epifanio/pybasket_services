59aed6bb90400337d988619c75e33572b3e03ac0
epifanio <epiesasha@me.com>
2022-05-06 12:23:24 +0200

    fix redis host environment

--- apps/pybasket_ui/dashboard/infrastructure/request_cache.py
+++ apps/pybasket_ui/dashboard/infrastructure/request_cache.py
@@ -4,15 +4,15 @@ import redis
 import json
 
 
-def get_data(transaction_id, redishost='redismod', password=None):
+def get_data(transaction_id, redishost="redis", password=None):
     r = redis.StrictRedis(host=redishost, password=password)
-    reply = json.loads(r.execute_command('JSON.GET', transaction_id))
+    reply = json.loads(r.execute_command("JSON.GET", transaction_id))
     return reply
 
 
-def set_data(transaction_id, data, redishost='redismod', password=None):
+def set_data(transaction_id, data, redishost="redis", password=None):
     r = redis.StrictRedis(host=redishost, password=password)
-    r.execute_command('JSON.SET', transaction_id, '.', json.dumps(data))
+    r.execute_command("JSON.SET", transaction_id, ".", json.dumps(data))
     return transaction_id
 
 
