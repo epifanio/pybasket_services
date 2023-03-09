7f9d5f244265a51be85da0fbebfc0b142ee55274
epifanio <epiesasha@me.com>
2023-03-09 08:25:34 +0000

    add compress2 method

--- apps/pybasket_api/worker.py
+++ apps/pybasket_api/worker.py
@@ -47,7 +47,7 @@ logger = get_task_logger(__name__)
 
 
 def validate_object(generated_yaml):
-    with open('info_object_schema.json') as f:
+    with open("info_object_schema.json") as f:
         information_object_schema = json.loads(f.read())
 
         # Load the information object.
@@ -57,7 +57,7 @@ def validate_object(generated_yaml):
             # Validate the information object.
             validate(instance=information_object, schema=information_object_schema)
 
-            print('If there are no exceptions then the info objected was validated!')
+            print("If there are no exceptions then the info objected was validated!")
             return True
 
 
@@ -167,42 +167,56 @@ def generate_spec(data, notebooks, transaction_id):
         }
         for i in data["notebooks"]
     ]
-    print(data['notebooks'])
-    notebooks = [{'name': i, 
-            'resource': data['notebooks'][i]['resource'],
-             'dependencies': [{'name':j['name'],
-                               # library dependencies are hardcoded to an emty array 
-                               # can be extended if really needed
-                               'dependencies':[], 
-                               'resource': j['resource'],
-                               'version': j['version']} for j in data['notebooks'][i]['dependencies']]} for i in data['notebooks']]
-    
+    print(data["notebooks"])
+    notebooks = [
+        {
+            "name": i,
+            "resource": data["notebooks"][i]["resource"],
+            "virtual_environment_type": data["notebooks"][i][
+                "virtual_environment_type"
+            ],
+            "dependencies": [
+                {
+                    "name": j["name"],
+                    # library dependencies are hardcoded to an emty array
+                    # can be extended if really needed
+                    "dependencies": [],
+                    "resource": j["resource"],
+                    "version": j["version"],
+                }
+                for j in data["notebooks"][i]["dependencies"]
+            ],
+        }
+        for i in data["notebooks"]
+    ]
+
     # context is hardocoded here
     # need to generate a uuid
     # dependencies for the dependencies are set to be empty
-    context = {'type':'VRE config', 
-           'id': str(uuid.uuid4())}
-    environment = {'provider':'PTEP', 
-               'processor':'JupyterHub'}
+    context = {"type": "VRE config", "id": str(uuid.uuid4())}
+    environment = {"provider": "PTEP", "processor": "JupyterHub"}
     templates = Jinja2Templates(directory="/app/templates")
-    #ff = templates.get_template("config/obj_tmpl.yaml").render(
+    # ff = templates.get_template("config/obj_tmpl.yaml").render(
     #    {"request": "request", "datasets": datasets, "notebooks": notebooks}
-    #)
+    # )
 
-    ff = templates.get_template("config/ptep_obj_tmpl.yaml").render({"request": 'request', 
-                                                            "datasets": datasets, 
-                                                            "notebooks": notebooks,
-                                                            "context": context,
-                                                            "environment": environment})
-
+    ff = templates.get_template("config/ptep_obj_tmpl.yaml").render(
+        {
+            "request": "request",
+            "datasets": datasets,
+            "notebooks": notebooks,
+            "context": context,
+            "environment": environment,
+        }
+    )
 
     print(ff)
     try:
-        with open('/app/config/info_object_schema.json') as f:
+        with open("/app/config/info_object_schema.json") as f:
             information_object_schema = json.loads(f.read())
             validate(instance=ff, schema=information_object_schema)
-    except ValidationError:
-        print('generated yaml not valid')
+    except ValidationError as e:
+        print("generated yaml not valid: ", e)
 
     with open(outfile, "w+") as yaml_file:
         ##yaml.dump(data, yaml_file, allow_unicode=True)
@@ -238,6 +252,145 @@ def generate_spec(data, notebooks, transaction_id):
 
 
 @celery_app.task
+def fake_compress2(data, notebooks, transaction_id):
+    # print(data, email_to)
+    # for i in data:
+    #    print('key:', i)
+    #    print('opendap_url:', data[i]['resources']['opendap'][0])
+    # print('to be sent to:', email_to)
+    rv = base64.b64encode(uuid.uuid4().bytes).decode("utf-8")
+    unique = re.sub(
+        r"[\=\+\/]", lambda m: {"+": "-", "/": "_", "=": ""}[m.group(0)], rv
+    )
+    filename = str(unique) + ".zip"
+    outfile = Path(download_dir, str(filename))
+    s = TimestampSigner("secret-key")
+    download_token = s.sign(filename).decode()
+    zip_file = zipfile.ZipFile(outfile, "a")
+
+    data = {"data": data, "notebooks": notebooks}
+    datasets = [
+        {
+            "id": i,
+            "title": data["data"][i]["title"],
+            "resource": data["data"][i]["resources"][
+                list(data["data"][i]["resources"].keys())[0]
+            ][0],
+            "type": list(data["data"][i]["resources"].keys())[0],
+        }
+        for i in data["data"]
+    ]
+    notebooks = [
+        {
+            "resource": data["notebooks"][i]["resource"],
+            "type": "jupyter",
+            "dependencies": [],
+        }
+        for i in data["notebooks"]
+    ]
+    print(data["notebooks"])
+    notebooks = [
+        {
+            "name": i,
+            "resource": data["notebooks"][i]["resource"],
+            "dependencies": [
+                {
+                    "name": j["name"],
+                    # library dependencies are hardcoded to an emty array
+                    # can be extended if really needed
+                    "dependencies": [],
+                    "resource": j["resource"],
+                    "version": j["version"],
+                }
+                for j in data["notebooks"][i]["dependencies"]
+            ],
+        }
+        for i in data["notebooks"]
+    ]
+
+    # context is hardocoded here
+    # need to generate a uuid
+    # dependencies for the dependencies are set to be empty
+    context = {"type": "VRE config", "id": str(uuid.uuid4())}
+    environment = {"provider": "PTEP", "processor": "JupyterHub"}
+    templates = Jinja2Templates(directory="/app/templates")
+    # ff = templates.get_template("config/obj_tmpl.yaml").render(
+    #    {"request": "request", "datasets": datasets, "notebooks": notebooks}
+    # )
+
+    ff = templates.get_template("config/ptep_obj_tmpl.yaml").render(
+        {
+            "request": "request",
+            "datasets": datasets,
+            "notebooks": notebooks,
+            "context": context,
+            "environment": environment,
+        }
+    )
+
+    print(ff)
+    try:
+        with open("/app/config/info_object_schema.json") as f:
+            information_object_schema = json.loads(f.read())
+            validate(instance=ff, schema=information_object_schema)
+    except ValidationError:
+        print("generated yaml not valid")
+
+    yaml_filename = str(unique) + ".yaml"
+    yaml_outfile = Path(download_dir, str(yaml_filename))
+    with open(yaml_outfile, "w+") as yaml_file:
+        ##yaml.dump(data, yaml_file, allow_unicode=True)
+        yaml_file.write(ff)
+    zip_file.write(yaml_outfile, os.path.basename(yaml_outfile))
+    datasets = data["data"]
+    for i in datasets:
+        # print("################## DATA  ############################## \n")
+        # print("data: ", data)
+        # print("#################  DATA[i] ############################### \n")
+        # print("data[i]: ", data[i])
+        # print("######################## i ######################## \n  i: ", i)
+        if "opendap" in datasets[i]["resources"]:
+            nc_url = datasets[i]["resources"]["opendap"][0]
+            logger.info(f"processing {i} in {filename}")
+            try:
+                with xr.open_dataset(nc_url, decode_cf=False) as ds:
+                    try:
+                        nc_name = nc_url.split("/")[-1]
+                        ds.to_netcdf(nc_name)
+                    except:
+                        logger.debug(f"failed processing {i}")
+                    try:
+                        zip_file.write(nc_name, os.path.basename(nc_name))
+                    except:
+                        logger.debug(f"failed processing {i}")
+                logger.info(f"Compressing {i} in {filename}")
+            except FileNotFoundError:
+                print(f"resource {nc_url} is not a valid opendap resource")
+    zip_file.close()
+    env = Environment(loader=FileSystemLoader("""/app/templates"""))
+    template = env.get_template("download/mail_download.html")
+    url = f"https://{api_host}/api/download/{download_token}"
+    output = template.render(data=data, date=dt.datetime.now().isoformat(), url=url)
+    data["download_url"] = url
+    transaction_id_data = transaction_id + "_data"
+    set_data(
+        transaction_id=transaction_id_data,
+        data=data,
+        redishost="redis",
+        password=os.environ["REDIS_PASSWORD"],
+    )
+    time.sleep(1)
+    status = {"status": True}
+    set_data(
+        transaction_id=transaction_id,
+        data=status,
+        redishost="redis",
+        password=os.environ["REDIS_PASSWORD"],
+    )
+    print(url, transaction_id, data)
+
+
+@celery_app.task
 def fake_compress(data, email_to, transaction_id):
     # print(data, email_to)
     # for i in data:
@@ -253,28 +406,31 @@ def fake_compress(data, email_to, transaction_id):
     s = TimestampSigner("secret-key")
     download_token = s.sign(filename).decode()
     zip_file = zipfile.ZipFile(outfile, "a")
+
     yaml_filename = str(unique) + ".yaml"
     yaml_outfile = Path(download_dir, str(yaml_filename))
     with open(yaml_outfile, "w+") as yaml_file:
         yaml.dump(data, yaml_file, allow_unicode=True)
     zip_file.write(yaml_outfile, os.path.basename(yaml_outfile))
+
     for i in data:
-        nc_url = data[i]["resources"]["opendap"][0]
-        logger.info(f"processing {i} in {filename}")
-        try:
-            with xr.open_dataset(nc_url, decode_cf=False) as ds:
-                try:
-                    nc_name = nc_url.split("/")[-1]
-                    ds.to_netcdf(nc_name)
-                except:
-                    logger.debug(f"failed processing {i}")
-                try:
-                    zip_file.write(nc_name, os.path.basename(nc_name))
-                except:
-                    logger.debug(f"failed processing {i}")
-            logger.info(f"Compressing {i} in {filename}")
-        except FileNotFoundError:
-            print(f"resource {nc_url} is not a valid opendap resource")
+        if "opendap" in data[i]["resources"]:
+            nc_url = data[i]["resources"]["opendap"][0]
+            logger.info(f"processing {i} in {filename}")
+            try:
+                with xr.open_dataset(nc_url, decode_cf=False) as ds:
+                    try:
+                        nc_name = nc_url.split("/")[-1]
+                        ds.to_netcdf(nc_name)
+                    except:
+                        logger.debug(f"failed processing {i}")
+                    try:
+                        zip_file.write(nc_name, os.path.basename(nc_name))
+                    except:
+                        logger.debug(f"failed processing {i}")
+                logger.info(f"Compressing {i} in {filename}")
+            except FileNotFoundError:
+                print(f"resource {nc_url} is not a valid opendap resource")
     zip_file.close()
     env = Environment(loader=FileSystemLoader("""/app/templates"""))
     template = env.get_template("download/mail_download.html")
