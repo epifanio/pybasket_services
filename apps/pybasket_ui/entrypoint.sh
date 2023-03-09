791bd954f5634764217433c9ddb7d8eb1d1d8692
epifanio <epiesasha@me.com>
2023-03-09 10:07:38 +0000

    no changes

--- apps/pybasket_ui/entrypoint.sh
+++ apps/pybasket_ui/entrypoint.sh
@@ -9,4 +9,8 @@ fi
 
 # bokeh serve --port ${PORT} --address 0.0.0.0 --allow-websocket-origin '*' ${PREFIX_PARAM} --log-level debug /pybasket_ui /SIE /GISPY /TS-Plot
 
-bokeh serve --port ${PORT} --address 0.0.0.0 --allow-websocket-origin ${ORIGIN} ${PREFIX_PARAM} --log-level debug /pybasket_ui /SIE /GISPY /TS-Plot /SeaIce
+# bokeh serve --port ${PORT} --address 0.0.0.0 --allow-websocket-origin ${ORIGIN} ${PREFIX_PARAM} --log-level debug /pybasket_ui /GISPY /TS-Plot /SIE
+
+bokeh serve --port ${PORT} --address 0.0.0.0 --allow-websocket-origin ${ORIGIN} ${PREFIX_PARAM} --log-level debug /pybasket_ui /GISPY /TS-Plot /SIE
+
+# /SeaIce
